98b9407f6b56359b447da6195ebe25b7bba3563f
ナッティ <hello@nutty.cc>
2020-01-14 17:19:56 -0700

    WTF? let and const are hoisted

--- web/ts/declarations.ts
+++ web/ts/declarations.ts
@@ -21,7 +21,8 @@ const { a: apple, b: banana } = foo
 console.log(apple)  // => 100
 console.log(banana) // => 200
 
-// You can also set default values in case a property is undefined.
+// You can also set default values in case a property is undefined, but maybe I
+// knew this because I've done it in function declarations.
 const { z = 'not defined' } = foo
 console.log(z)
 
@@ -31,3 +32,22 @@ let b: number
 ;({ a, b } = foo)
 console.log(a)
 console.log(b)
+
+function tdz () {
+	//  ^
+    //  |
+	//
+	// This space between the beginning of the block and the declaration of 'a'
+	// is called a "temporal dead zone". TECHNICALLY, let and const declarations
+	// DO hoist, but don't get initialized. And if you try to access them in the
+	// temporal dead zone, you'll get a ReferenceError.
+	//
+	// The temporal dead zone is where you try to access a variable that has
+	// been declared, but it doesn't have a value, not even `undefined`. In
+	// contrast, var declarations are hoisted and initialized to `undefined`.
+	//
+	//  |
+	//  v
+	let a = 'hi'
+	console.log(a)
+}
