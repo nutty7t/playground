4d89fd30539254f28a3c45a46d84ec4f5f2a3054
ナッティ <hello@nutty.cc>
2020-01-27 14:14:48 -0700

    Learn about string, homogeneous, and const enums

--- web/ts/enums.ts
+++ web/ts/enums.ts
@@ -45,3 +45,49 @@ console.log(Conflict.Bar)           // => 2
 console.log(Conflict.Baz)           // => 2
 console.log(Conflict[2])            // => Baz
 console.log(Conflict[Conflict.Bar]) // => Baz, NOT BIJECTIVE!
+
+// All members of string (or heterogeneous) enums must be constant-initialized.
+enum StringEnum {
+	SS = 'SS (string)',
+	S  = 'S (string)',
+	A  = 'A (string)',
+	B  = 'B (string)',
+	C  = 'C (string)',
+	D  = 'D (string)',
+	F  = 'F (string)',
+
+	// make this homogeneous
+	Test = 0
+}
+
+console.log(StringEnum)
+console.log(StringEnum.S)
+
+// String enums do not get reverse mappings at all.
+console.log(StringEnum[StringEnum.S])
+
+// However, enum members with numeric values DO get reverse mappings.
+console.log(StringEnum[StringEnum.Test])
+
+// equivalent to 'SS' | 'S' | ... | 'F' | 'Test'
+//
+// This looks kind of weird doesn't it? keyof operates on types, but StringEnum
+// is a value. The typeof operator takes a value and produces its type, so you
+// can use the following.
+type Rank = keyof typeof StringEnum
+const rank: Rank = 'SS'
+// const rank: Rank = 'bad value' // doesn't type check
+
+// *const* enums restrict computed members.
+// const external = 'no no'
+const enum ConstEnum {
+	A = 1,
+	B = 2,
+	// C = external, // bad
+	C = A + B + 3
+}
+
+// You can't output const enums because they are completely inlined.
+// After TS compilation, they don't exist.
+
+// console.log(ConstEnum)
