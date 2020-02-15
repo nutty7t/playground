7c8abea543677d17023dd3c6f57b95b9c47e8d4d
ナッティ <hello@nutty.cc>
2020-02-14 17:27:06 -0700

    Start reading the Rust book

---
+++ rust/book/guessing_game/src/main.rs
@@ -0,0 +1,48 @@
+// This seems similar to the C++'s namespaces.
+// If we omit the `use` statement, we can refer to
+// std::io::stdin instead.
+//
+// Or is it more like ES6 modules?
+// > import { thread_rng, Rng } from rand
+// > import { io } from std
+//
+// Rng is a trait. I hear that traits are similar to type classes.
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+	println!("Guess the number!");
+
+	let secret_number = thread_rng().gen_range(1, 101);
+
+	println!("The secret number is: {}", secret_number);
+
+	println!("Please input your guess.");
+
+	// Variables are immutable by default.
+	// To make them mutable, we must include the `mut` keyword.
+	let mut guess = String::new();
+
+	io::stdin()
+		// References are immutable by default, so we need to
+		// use `&mut` to make them mutable.
+		//
+		// std::io::stdin returns an io::Result type.
+		//
+		//   type Result<T> = Result<T, Error>;
+		//
+		// This reminds me a lot of Haskell's Either type.
+		.read_line(&mut guess)
+		// expect unwraps a result if the instance of io::Result is Ok.
+		// Otherise it will panic with an error message if the instance is Err.
+		.expect("Failed to read line");
+
+	println!("You guessed: {}", guess);
+
+	match guess.cmp(&secret_number) {
+		Ordering::Less => println!("Too small!"),
+		Ordering::Greater => println!("Too big!"),
+		Ordering::Equal => println!("You win!"),
+	}
+}
