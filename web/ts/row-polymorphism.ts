1542ef3653a6cb8e698038ea46c945896627ddb8
ナッティ <hello@nutty.cc>
2020-06-03 15:32:42 -0700

    More row polymorphism

--- web/ts/row-polymorphism.ts
+++ web/ts/row-polymorphism.ts
@@ -33,4 +33,14 @@ test2.baz // [tsserver 2339] [E] Property 'baz' does not exist on type 'Example'
 
 // The type of test2 is inferred to be `Example` and not `Example2`.
 // Some type information is lost, which makes me think that this is structural
-// subtyping and not row polymorphism.
+// subtyping and not row polymorphism. There's no polymorphic type variable
+// that preserves type information. But maybe we can do something else by
+// combining structural subtyping with parametric polymorphism
+
+const processFoo2 = <T extends Example>(e: T): T => {
+	e.bar++
+	return e
+}
+
+const test3 = processFoo2(test)
+test3.baz // WORKS!
